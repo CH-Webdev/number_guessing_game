9068548fa57d7479b49118497fadc69ef217a2d7
CH-Webdev <94474027+CH-Webdev@users.noreply.github.com>
2022-03-23 16:38:43 -0500

    Add files via upload

---
+++ number_guessing_game/main.py
@@ -0,0 +1,23 @@
+import random
+from re import I
+
+random_num = random.randint(1,9)
+guess = 0
+i = 0
+while guess != random_num and guess != "exit":
+    guess = input("Guess a number 1 through 9: ")
+
+    if guess == "exit":
+        break
+
+    guess = int(guess)
+    i += 1
+
+    if guess < random_num:
+        print("Too low")
+    elif guess > random_num:
+        print("Too high")
+    else:
+        print("You Win!")
+        print("Took you only", i, "tries!")
+input()
